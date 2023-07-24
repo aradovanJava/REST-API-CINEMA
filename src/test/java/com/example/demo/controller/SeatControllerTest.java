dd7ef8b7c78f4ffd853e6b046b2bc7f161fa652f
aradovanJava <aleksander.radovan@gmail.com>
2023-07-24 12:16:30 +0200

    First jUnit test implemented.

---
+++ src/test/java/com/example/demo/controller/SeatControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SeatControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void shouldReturnAllSeatsFromFile() throws Exception {
+        this.mockMvc.perform(get("/cinema/seat"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(16)));
+    }
+}
